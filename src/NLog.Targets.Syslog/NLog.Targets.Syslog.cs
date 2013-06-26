e65edde12623e718a4158245aa034453d45003d2
Graffen <jesper@graffen.dk>
2013-06-26 09:44:57 +0200

    Moved enums to separate files
    Updated copyright year in header of NLog.Targets.Syslog.cs

--- src/NLog.Targets.Syslog/NLog.Targets.Syslog.cs
+++ src/NLog.Targets.Syslog/NLog.Targets.Syslog.cs
@@ -1,7 +1,7 @@
 ﻿////
 //   NLog.Targets.Syslog
 //   ------------------------------------------------------------------------
-//   Copyright 2010 Jesper Hess Nielsen <jesper@graffen.dk>
+//   Copyright 2013 Jesper Hess Nielsen <jesper@graffen.dk>
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -32,209 +32,9 @@ namespace NLog.Targets
     /// <summary>
     /// This class enables logging to a unix-style syslog server using NLog.
     /// </summary>
-    
     [Target("Syslog")]
     public class Syslog : Target
     {
-        /// <summary>
-        /// Initializes a new instance of the Syslog class
-        /// </summary>
-        public Syslog()
-        {
-            // Sensible defaults...
-            SyslogServer = "127.0.0.1";
-            Port = 514;
-            Sender = Assembly.GetCallingAssembly().GetName().Name;
-            Facility = SyslogFacility.Local1;
-            Protocol = ProtocolType.Udp;
-        }
-
-        #region Enumerations
-        /// <summary>
-        /// syslog severities
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// The syslog severities.
-        /// </para>
-        /// </remarks>
-        public enum SyslogSeverity
-        {
-            /// <summary>
-            /// system is unusable
-            /// </summary>
-            Emergency = 0,
-
-            /// <summary>
-            /// action must be taken immediately
-            /// </summary>
-            Alert = 1,
-
-            /// <summary>
-            /// critical conditions
-            /// </summary>
-            Critical = 2,
-
-            /// <summary>
-            /// error conditions
-            /// </summary>
-            Error = 3,
-
-            /// <summary>
-            /// warning conditions
-            /// </summary>
-            Warning = 4,
-
-            /// <summary>
-            /// normal but significant condition
-            /// </summary>
-            Notice = 5,
-
-            /// <summary>
-            /// informational messages
-            /// </summary>
-            Informational = 6,
-
-            /// <summary>
-            /// debug-level messages
-            /// </summary>
-            Debug = 7
-        };
-
-        /// <summary>
-        /// syslog facilities
-        /// </summary>
-        /// <remarks>
-        /// <para>
-        /// The syslog facilities
-        /// </para>
-        /// </remarks>
-        public enum SyslogFacility
-        {
-            /// <summary>
-            /// kernel messages
-            /// </summary>
-            Kernel = 0,
-
-            /// <summary>
-            /// random user-level messages
-            /// </summary>
-            User = 1,
-
-            /// <summary>
-            /// mail system
-            /// </summary>
-            Mail = 2,
-
-            /// <summary>
-            /// system daemons
-            /// </summary>
-            Daemons = 3,
-
-            /// <summary>
-            /// security/authorization messages
-            /// </summary>
-            Authorization = 4,
-
-            /// <summary>
-            /// messages generated internally by syslogd
-            /// </summary>
-            Syslog = 5,
-
-            /// <summary>
-            /// line printer subsystem
-            /// </summary>
-            Printer = 6,
-
-            /// <summary>
-            /// network news subsystem
-            /// </summary>
-            News = 7,
-
-            /// <summary>
-            /// UUCP subsystem
-            /// </summary>
-            Uucp = 8,
-
-            /// <summary>
-            /// clock (cron/at) daemon
-            /// </summary>
-            Clock = 9,
-
-            /// <summary>
-            /// security/authorization  messages (private)
-            /// </summary>
-            Authorization2 = 10,
-
-            /// <summary>
-            /// ftp daemon
-            /// </summary>
-            Ftp = 11,
-
-            /// <summary>
-            /// NTP subsystem
-            /// </summary>
-            Ntp = 12,
-
-            /// <summary>
-            /// log audit
-            /// </summary>
-            Audit = 13,
-
-            /// <summary>
-            /// log alert
-            /// </summary>
-            Alert = 14,
-
-            /// <summary>
-            /// clock daemon
-            /// </summary>
-            Clock2 = 15,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local0 = 16,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local1 = 17,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local2 = 18,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local3 = 19,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local4 = 20,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local5 = 21,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local6 = 22,
-
-            /// <summary>
-            /// reserved for local use
-            /// </summary>
-            Local7 = 23
-        }
-
-        #endregion Enumerations
-
-        #region Public properties
         /// <summary>
         /// Sets the IP Address or Host name of your Syslog server
         /// </summary>
@@ -266,7 +66,19 @@ namespace NLog.Targets
         /// Sets the syslog server protocol (tcp/udp) 
         /// </summary>
         public ProtocolType Protocol { get; set; }
-        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of the Syslog class
+        /// </summary>
+        public Syslog()
+        {
+            // Sensible defaults...
+            SyslogServer = "127.0.0.1";
+            Port = 514;
+            Sender = Assembly.GetCallingAssembly().GetName().Name;
+            Facility = SyslogFacility.Local1;
+            Protocol = ProtocolType.Udp;
+        }
 
         /// <summary>
         /// This is where we hook into NLog, by overriding the Write method. 
---
+++ src/NLog.Targets.Syslog/SyslogFacility.cs
@@ -0,0 +1,133 @@
+﻿namespace NLog.Targets
+{
+    /// <summary>
+    /// syslog facilities
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The syslog facilities
+    /// </para>
+    /// </remarks>
+    public enum SyslogFacility
+    {
+        /// <summary>
+        /// kernel messages
+        /// </summary>
+        Kernel = 0,
+
+        /// <summary>
+        /// random user-level messages
+        /// </summary>
+        User = 1,
+
+        /// <summary>
+        /// mail system
+        /// </summary>
+        Mail = 2,
+
+        /// <summary>
+        /// system daemons
+        /// </summary>
+        Daemons = 3,
+
+        /// <summary>
+        /// security/authorization messages
+        /// </summary>
+        Authorization = 4,
+
+        /// <summary>
+        /// messages generated internally by syslogd
+        /// </summary>
+        Syslog = 5,
+
+        /// <summary>
+        /// line printer subsystem
+        /// </summary>
+        Printer = 6,
+
+        /// <summary>
+        /// network news subsystem
+        /// </summary>
+        News = 7,
+
+        /// <summary>
+        /// UUCP subsystem
+        /// </summary>
+        Uucp = 8,
+
+        /// <summary>
+        /// clock (cron/at) daemon
+        /// </summary>
+        Clock = 9,
+
+        /// <summary>
+        /// security/authorization  messages (private)
+        /// </summary>
+        Authorization2 = 10,
+
+        /// <summary>
+        /// ftp daemon
+        /// </summary>
+        Ftp = 11,
+
+        /// <summary>
+        /// NTP subsystem
+        /// </summary>
+        Ntp = 12,
+
+        /// <summary>
+        /// log audit
+        /// </summary>
+        Audit = 13,
+
+        /// <summary>
+        /// log alert
+        /// </summary>
+        Alert = 14,
+
+        /// <summary>
+        /// clock daemon
+        /// </summary>
+        Clock2 = 15,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local0 = 16,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local1 = 17,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local2 = 18,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local3 = 19,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local4 = 20,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local5 = 21,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local6 = 22,
+
+        /// <summary>
+        /// reserved for local use
+        /// </summary>
+        Local7 = 23
+    }
+}
