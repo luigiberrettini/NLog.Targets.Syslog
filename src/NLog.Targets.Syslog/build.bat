f750ed541d7cf9a8f7bfd1c8b330cbd77364efc3
Jesper Hess Nielsen <jesper@graffen.dk>
2014-01-20 21:02:13 +0100

    Targeted Nuget to build from project file.

--- src/NLog.Targets.Syslog/build.bat
+++ src/NLog.Targets.Syslog/build.bat
@@ -1,4 +1,6 @@
+@echo off
+echo Building package...
 rmdir /S /Q deploy
-..\..\tools\NuGet.exe pack pkg\NLog.Targets.Syslog.nuspec
+..\..\tools\NuGet.exe pack NLog.Targets.Syslog.csproj -Prop Configuration=Release
 mkdir deploy
 for /f %%a in ('dir /b /s .\*.nupkg') do call move /Y %%a deploy
--- src/NLog.Targets.Syslog/buildtopublic.bat
+++ src/NLog.Targets.Syslog/buildtopublic.bat
@@ -1,5 +1,4 @@
-rmdir /S /Q deploy
-..\..\tools\NuGet.exe pack pkg\NLog.Targets.Syslog.nuspec
-mkdir deploy
-for /f %%a in ('dir /b /s .\*.nupkg') do call move /Y %%a deploy
+@echo off
+call build.bat
+echo Publishing package...
 for /f %%a in ('dir /b /s .\deploy\*.nupkg') do call ..\..\tools\NuGet.exe push %%a
