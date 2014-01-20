192a34ca9a6932f41ba29971f20a6e059306f6ee
Jesper Hess Nielsen <jesper@graffen.dk>
2014-01-20 21:06:18 +0100

    Added auto-incrementing version number to AssemblyInfo.cs

--- src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.Targets.Syslog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The NLog Syslog Target is a custom target for NLog version 2.0 allowing you to send logging messages to a UNIX-style Syslog server.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Jesper Hess Nielsen, Joe Fitzgerald, Nicolas Graziano and others")]
 [assembly: AssemblyProduct("NLog.Targets.Syslog")]
-[assembly: AssemblyCopyright("Copyright © Jesper Hess Nielsen 2013")]
+[assembly: AssemblyCopyright("Copyright © Jesper Hess Nielsen 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.*")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
