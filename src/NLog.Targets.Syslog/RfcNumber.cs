33fea087186abab4e7020dc12657e779f9ef44b4
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-04-13 19:46:51 +0200

    Add XML comment to RfcNumber enum

--- src/NLog.Targets.Syslog/RfcNumber.cs
+++ src/NLog.Targets.Syslog/RfcNumber.cs
@@ -2,6 +2,7 @@
 namespace NLog.Targets
 // ReSharper restore CheckNamespace
 {
+    /// <summary>The Syslog protocol RFC to be followed</summary>
     public enum RfcNumber
     {
         Rfc3164 = 3164,
