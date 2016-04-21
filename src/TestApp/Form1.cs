facea900da61a33bd89f9ff091cf66449e75efc5
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-04-21 18:12:33 +0200

    Add brackets in switch cases

--- src/TestApp/Form1.cs
+++ src/TestApp/Form1.cs
@@ -22,25 +22,39 @@ namespace TestApp
             switch (btn.Name)
             {
                 case "buttonTrace":
+                {
                     Logger.Trace("This is a sample trace message");
                     break;
+                }
                 case "buttonDebug":
+                {
                     Logger.Debug("This is a sample debug message");
                     break;
+                }
                 case "buttonInfo":
+                {
                     Logger.Info("This is a sample info message");
                     break;
+                }
                 case "buttonWarn":
+                {
                     Logger.Warn("This is a sample warn message");
                     break;
+                }
                 case "buttonError":
+                {
                     Logger.Error("This is a sample error message");
                     break;
+                }
                 case "buttonFatal":
+                {
                     Logger.Fatal("This is a sample fatal message");
                     break;
+                }
                 default:
+                {
                     throw new InvalidOperationException($"Button name '{btn.Name}' is not supported");
+                }
             }
         }
     }
