b6aa93d8125574aff092ada731f1e8a70492fe38
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-09-30 11:25:14 +0200

    Clean-up assembly info files

--- src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
@@ -1,35 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NLog.Targets.Syslog")]
-[assembly: AssemblyDescription("The NLog Syslog Target is a custom target for NLog version 4.3.2 allowing you to send logging messages to a Unix-style Syslog server.")]
+[assembly: AssemblyDescription("The NLog Syslog Target is a custom target for NLog version 4.3.7 allowing you to send logging messages to a Unix-style Syslog server.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jesper Hess Nielsen, Joe Fitzgerald, Nicolas Graziano and others")]
 [assembly: AssemblyProduct("NLog.Targets.Syslog")]
 [assembly: AssemblyCopyright("Copyright © Jesper Hess Nielsen 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d3c3cc40-79d6-409e-aadc-60729f57099c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.*")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
