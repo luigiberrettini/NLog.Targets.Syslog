b2c7dc546fc55573d0ba5b0c67d880ffcc1cfc7e
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-12-14 14:51:38 +0100

    Add package restore and use specific MSBuild version

--- src/NLog.Targets.Syslog/build.bat
+++ src/NLog.Targets.Syslog/build.bat
@@ -1,7 +1,19 @@
 @echo off
-echo Building package...
-rmdir /S /Q deploy
-msbuild NLog.Targets.Syslog.csproj /t:Build /p:Configuration=Release /property:Platform=AnyCPU /v:minimal /nologo
-..\..\tools\NuGet.exe pack NLog.Targets.Syslog.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
+
+if not exist ..\packages (
+    set Configuration=Release
+    set Platform=AnyCPU
+    echo Restoring dependency packages...
+    ..\..\tools\NuGet.exe restore ..\NLog.Targets.Syslog.sln -msbuildversion 14
+)
+
+echo Building...
+@rmdir /S /Q deploy
+set MSBuildExe="%ProgramFiles%\MSBuild\14.0\Bin\MsBuild.exe"
+if exist "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" set MSBuildExe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe"
+%MSBuildExe% NLog.Targets.Syslog.csproj /t:Build /p:Configuration=Release /property:Platform=AnyCPU /v:minimal /nologo
+
+echo Creating NuGet package
+..\..\tools\NuGet.exe pack NLog.Targets.Syslog.csproj -msbuildversion 14 -Prop Configuration=Release -Prop Platform=AnyCPU
 mkdir deploy
 for /f %%a in ('dir /b /s .\*.nupkg') do call move /Y %%a deploy
