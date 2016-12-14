7ef5e2c4dcac681e4d64c62e4cfcb38621abf191
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-12-14 14:58:11 +0100

    Use semantic versioning (manual) for NuGet package and assembly file version

--- src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
@@ -10,5 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
