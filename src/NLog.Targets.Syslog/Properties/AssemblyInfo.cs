eeef9ed6dcff6c828067c7832c0cb7d67782243d
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-01-14 14:44:11 +0100

    Bump version to 3.0.2

--- src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.2")]
