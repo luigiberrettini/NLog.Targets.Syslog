85c04a3eb06d6e49af52f5fd72bd473e2bd0b7b3
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-01-17 20:15:44 +0100

    Update assembly description and authors

--- src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Syslog/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NLog.Targets.Syslog")]
-[assembly: AssemblyDescription("The NLog Syslog Target is a custom target for NLog version 4.3.7 allowing you to send logging messages to a Unix-style Syslog server.")]
+[assembly: AssemblyDescription("The NLog Syslog Target is a custom target for NLog version 4.3.7 and later allowing you to send logging messages to a Unix-style Syslog server.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jesper Hess Nielsen, Joe Fitzgerald, Nicolas Graziano and others")]
+[assembly: AssemblyCompany("Jesper Hess Nielsen, Luigi Berrettini and others")]
 [assembly: AssemblyProduct("NLog.Targets.Syslog")]
 [assembly: AssemblyCopyright("Copyright © Jesper Hess Nielsen 2014")]
 [assembly: AssemblyTrademark("")]
