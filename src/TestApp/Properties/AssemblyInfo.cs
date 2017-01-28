0c906105681c386a75b2ffcef7e0ec215690b333
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-01-28 20:21:37 +0100

    Use versioning appropriately (#70 closes #65)
     - Use Assembly*Version attributes appropriately
     - Get version from AssemblyInformationalVersion
     - Base AppVeyor build version on AssemblyInformationalVersion
     - Patch AssemblyFileVersion during AppVeyor builds

--- src/TestApp/Properties/AssemblyInfo.cs
+++ src/TestApp/Properties/AssemblyInfo.cs
@@ -11,3 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2013 - present by Jesper Hess Nielsen, Luigi Berrettini and others: https://github.com/graffen/NLog.Targets.Syslog/graphs/contributors")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
