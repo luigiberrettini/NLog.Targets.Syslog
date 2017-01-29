723cdfe30c89e291f40839788a8b0c5f51f3029b
Luigi Berrettini <luigi.berrettini@gmail.com>
2017-01-29 21:14:38 +0100

    Enable keep-alive configuration

---
+++ src/NLog.Targets.Syslog/MessageSend/KeepAlive.cs
@@ -0,0 +1,52 @@
+// Licensed under the BSD license
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace NLog.Targets.Syslog.Settings
+{
+    // Win32 header file: Mstcpip.h
+    //
+    // struct tcp_keepalive
+    // {
+    //     ULONG onoff;
+    //     ULONG keepalivetime;
+    //     ULONG keepaliveinterval;
+    // };
+    internal class KeepAlive
+    {
+        private readonly int uintSize;
+        private readonly int onOffOffset;
+        private readonly int timeOffset;
+        private readonly int intervalOffset;
+        private readonly int structSize;
+
+        public uint OnOff { get; }
+
+        public uint Time { get; }
+
+        public uint Interval { get; }
+
+        public KeepAlive(KeepAliveConfig keepAliveConfig)
+        {
+            uintSize = Marshal.SizeOf(typeof(uint));
+            onOffOffset = 0;
+            timeOffset = uintSize;
+            intervalOffset = 2 * uintSize;
+            structSize = 3 * uintSize;
+            OnOff = (uint)(keepAliveConfig.On ? 1 : 0);
+            Time = (uint)keepAliveConfig.Time;
+            Interval = (uint)keepAliveConfig.Interval;
+        }
+        
+        public byte[] ToByteArray()
+        {
+            byte[] keepAliveSettings = new byte[structSize];
+            BitConverter.GetBytes(OnOff).CopyTo(keepAliveSettings, onOffOffset);
+            BitConverter.GetBytes(Time).CopyTo(keepAliveSettings, timeOffset);
+            BitConverter.GetBytes(Interval).CopyTo(keepAliveSettings, intervalOffset);
+            return keepAliveSettings;
+        }
+    }
+}
---
+++ src/NLog.Targets.Syslog/Settings/KeepAliveConfig.cs
@@ -0,0 +1,31 @@
+// Licensed under the BSD license
+// See the LICENSE file in the project root for more information
+
+namespace NLog.Targets.Syslog.Settings
+{
+    /// <summary>KeepAlive configuration</summary>
+    /// <remarks>The number of keep-alive probes (data retransmissions) is set to 10 and cannot be changed</remarks>
+    public class KeepAliveConfig
+    {
+        private const int DefaultTime = 500;
+        private const int DefaultInterval = 100;
+
+        /// <summary>Whether to use keep-alive or not</summary>
+        public bool On { get; set; }
+
+        /// <summary>The timeout, in milliseconds, with no activity until the first keep-alive packet is sent</summary>
+        /// <remarks>The default value, on TCP socket initialization, is 2 hours</remarks>
+        public int Time { get; set; }
+
+        /// <summary>The interval, in milliseconds, between when successive keep-alive packets are sent if no acknowledgement is received</summary>
+        /// <remarks>The default value, on TCP socket initialization, is 1 second</remarks>
+        public int Interval { get; set; }
+
+        public KeepAliveConfig()
+        {
+            On = true;
+            Time = DefaultTime;
+            Interval = DefaultInterval;
+        }
+    }
+}
