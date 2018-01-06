443cf6a3d68405c04d847626fc5325c4cb89daf2
Luigi Berrettini <luigi.berrettini@gmail.com>
2018-01-06 17:26:35 +0100

    Target net452 instead of net45 (#112 closes #111)
    * Replace net45 (no more supported by MS) with net452
    * Refactor build scripts
    * Update NLog for netstandard2.0 to 4.5.0-rc03

--- tools/build.sh
+++ tools/build.sh
@@ -1,40 +1,65 @@
 #!/bin/bash
 
 function findPackageAssembly {
-    dir=$1
-    packageName=$2
-    assemblyName=$3
-    echo $(find "$dir/$packageName" -name "$assemblyName")
+    local resultvar=$1
+    local dir=$2
+    local packageName=$3
+    local packageVersion=$4
+    local assemblyName=$5
+    local packageAssembly=$(find "$dir/$packageName/$packageVersion" -name "$assemblyName" 2>/dev/null)
+    eval $resultvar="$packageAssembly"
 }
 
 function installPackage {
-    dir=$1
-    packageName=$2
-    echo "Installing $packageName in $dir..."
+    local dir=$1
+    local packageName=$2
+    local packageVersion=$3
+    echo "Installing '$packageName' version '$packageVersion' in '$dir'..."
     mkdir -p $dir
-    contents='<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netstandard1.5</TargetFramework></PropertyGroup></Project>'
-    proj="$packagesDir/project.csproj"
+    local contents='<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netstandard1.5</TargetFramework></PropertyGroup></Project>'
+    local proj="$dir/prj.csproj"
     echo $contents > $proj
-    dotnet add "$proj" package $packageName --package-directory $dir
+    dotnet add "$proj" package $packageName --version $packageVersion --package-directory $dir
+}
+
+function getPackageAssemblyPath {
+    local resultvar=$1
+    local scriptArgsName=$2[@]
+    local packagesDir=$3
+    local packageName=$4
+    local packageVersionArgSwitch=$5
+    local assemblyName=$6
+    local scriptArgs=("${!scriptArgsName}")
+    for item in ${scriptArgs[@]}; do
+        if [[ $item == $packageVersionArgSwitch* ]]; then
+            local packageVersion=${item/$packageVersionArgSwitch/}
+        fi
+    done
+    if [ -z $packageVersion ]; then
+        local packageVersionSearchUrl="https://api-v2v3search-0.nuget.org/autocomplete?id=$packageName"
+        local packageVersion=$(curl --silent $packageVersionSearchUrl | jq .data[-1] | sed -e 's/^"//' -e 's/"$//')
+    fi
+    findPackageAssembly assemblyPath $buildScriptPackagesDir $packageName $packageVersion $assemblyName
+    if [ ! -f "$assemblyPath" ]; then
+        installPackage $buildScriptPackagesDir $packageName $packageVersion
+        findPackageAssembly assemblyPath $buildScriptPackagesDir $packageName $packageVersion $assemblyName
+        if [ ! -f "$assemblyPath" ]; then
+            echo "Cannot find '$assemblyName' in directory '$packagesDir' for package '$packageName' of version '$packageVersion'"
+            exit 1
+        fi
+    fi
+    eval $resultvar="$assemblyPath"
 }
 
 scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 rootDir=$scriptDir; while [ $(ls -Ald "$rootDir/.git" 2>/dev/null | wc -l) -eq 0 ]; do rootDir="$rootDir/.."; done
-srcDir="$rootDir/src"
-toolsDir="$rootDir/tools"
-packagesDir="$toolsDir/cake"
-cakePackageName='cake.coreclr'
-cakeAssemblyName='Cake.dll'
-cakeAssemblyPath=$(findPackageAssembly $packagesDir $cakePackageName $cakeAssemblyName)
-if [ ! -f "$cakeAssemblyPath" ]; then
-    installPackage $packagesDir $cakePackageName
-    cakeAssemblyPath=$(findPackageAssembly $packagesDir $cakePackageName $cakeAssemblyName)
-    if [ ! -f "$cakeAssemblyPath" ]; then
-        echo "Cannot find Cake assembly '$cakeDll'"
-        exit 1
-    fi
-fi
+buildScriptPackagesDir="$rootDir/tools/pkgs"
+allArgs=$@
+getPackageAssemblyPath cakeAssemblyPath allArgs $buildScriptPackagesDir 'cake.coreclr' '--cakePackageVersion=' 'Cake.dll'
 cakeScript="$scriptDir/build.cake"
+gitRemote=$(git remote get-url origin | sed -e 's/.git$//')
+srcDir="$rootDir/src"
+artifactsDir="$rootDir/tools/artifacts"
+commitHash=$(git rev-parse --short HEAD)
 
-dotnet "$cakeAssemblyPath" "$cakeScript" "-srcDir=$srcDir" "-toolsDir=$toolsDir" "-commitHash=$(git rev-parse --short HEAD)" "$@"
+dotnet "$cakeAssemblyPath" "$cakeScript" "--gitRemote=$gitRemote" "--srcDir=$srcDir" "--artifactsDir=$artifactsDir" "--commitHash=$commitHash" "$@"
