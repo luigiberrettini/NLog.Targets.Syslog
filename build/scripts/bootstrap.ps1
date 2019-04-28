6c29abb6610d2c324e5457ae4dd88a2ca2f03a99
Luigi Berrettini <luigi.berrettini@gmail.com>
2019-04-29 00:00:46 +0100

    Build using Cake .NET Core tool

--- build/scripts/bootstrap.ps1
+++ build/scripts/bootstrap.ps1
@@ -1,25 +1,28 @@
-function Find-PackageAssembly {
-    Param($dir, $packageName, $packageVersion, $assemblyName)
-    $packagePath = Join-Path $dir (Join-Path $packageName $packageVersion)
-    if (-Not (Test-Path $packagePath)) {
+function Find-ToolAssembly {
+    Param($dir, $packageName, $packageVersion, $toolName, $assemblyName)
+    $toolDir = Join-Path $dir (Join-Path $packageName $packageVersion)
+    if (-Not (Test-Path $toolDir)) {
         return $null
     }
-    return (Get-ChildItem -Force -Recurse $packagePath -Filter $assemblyName).FullName
+    if (-Not (Get-ChildItem -Force -Recurse $toolDir -Filter $assemblyName)) {
+        return $null
+    }
+    return (Get-ChildItem -Force $toolDir -Filter "$toolName.exe").FullName
 }
 
-function Install-Package {
+function Install-Tool {
     Param($dir, $packageName, $packageVersion)
     Write-Host "Installing '$packageName' version '$packageVersion' in '$dir'..."
-    New-Item -Type Directory -Path $dir -Force | Out-Null
-    $contents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netstandard1.5</TargetFramework></PropertyGroup></Project>'
-    $proj = Join-Path $dir 'prj.csproj'
-    Out-File -InputObject $contents -FilePath $proj
-    $addPackageOutput = $(dotnet add "$proj" package $packageName --version $packageVersion --package-directory $dir)
-    Write-Host $addPackageOutput
+    $toolDir = Join-Path $dir (Join-Path $packageName $packageVersion)
+    if (Test-Path $toolDir) {
+        Remove-Item -Force -Recurse -Path $toolDir
+    }
+    New-Item -Type Directory -Path $toolDir -Force | Out-Null
+    dotnet tool install $packageName --tool-path $toolDir --version $packageVersion | Out-Null
 }
 
-function Get-PackageAssemblyPath {
-    Param($scriptArgs, $packagesDir, $packageName, $packageVersionArgSwitch, $assemblyName)
+function Get-ToolAssemblyPath {
+    Param($scriptArgs, $toolsDir, $packageName, $packageVersionArgSwitch, $toolName, $assemblyName)
     $packageVersionArg = $scriptArgs | Where-Object { $_ -ne $null -and $_.StartsWith($packageVersionArgSwitch) }
     if ($packageVersionArg -eq $null) {
         $packageVersion = (Find-Package -Source 'http://www.nuget.org/api/v2' -Name $packageName).Version
@@ -27,26 +30,29 @@ function Get-PackageAssemblyPath {
     else {
         $packageVersion = $packageVersionArg.Split('=')[1]
     }
-    $assemblyPath = Find-PackageAssembly $packagesDir $packageName $packageVersion $assemblyName
+    $assemblyPath = Find-ToolAssembly $toolsDir $packageName $packageVersion $toolName $assemblyName
     if (($assemblyPath -eq $null) -or -Not (Test-Path $assemblyPath)) {
-        Install-Package $packagesDir $packageName $packageVersion
-        $assemblyPath = Find-PackageAssembly $packagesDir $packageName $packageVersion $assemblyName
+        Install-Tool $toolsDir $packageName $packageVersion
+        $assemblyPath = Find-ToolAssembly $toolsDir $packageName $packageVersion $toolName $assemblyName
         if (($assemblyPath -eq $null) -or -Not (Test-Path $assemblyPath)) {
-            Throw "Cannot find '$assemblyName' in directory '$packagesDir' for package '$packageName' of version '$packageVersion'"
+            Throw "Cannot find '$toolName' and/or '$assemblyName' in directory '$toolsDir' for package '$packageName' of version '$packageVersion'"
             exit 1
         }
     }
     return $assemblyPath
 }
 
-$scriptDir = $PSScriptRoot
-$rootDir = $scriptDir; while ($(Get-ChildItem -Force -Filter '.git' $rootDir | Measure-Object).Count -eq 0) { $rootDir = Join-Path $rootDir '..' }
-$buildScriptPackagesDir = Join-Path $rootDir (Join-Path 'build' 'tools')
-$cakeAssemblyPath = Get-PackageAssemblyPath $args $buildScriptPackagesDir 'cake.coreclr' '--cakePackageVersion=' 'Cake.dll'
-$cakeScript = Join-Path $scriptDir 'cake.cs'
-$gitRemote=$(git remote get-url origin) -replace '.git$', ''
+$rootDir = $PSScriptRoot; while ($(Get-ChildItem -Force -Filter '.git' $rootDir | Measure-Object).Count -eq 0) { $rootDir = (Get-Item (Join-Path $rootDir '..')).FullName }
 $srcDir = Join-Path $rootDir 'src'
-$artifactsDir = Join-Path $rootDir (Join-Path 'build' 'artifacts')
-$commitHash=$(git rev-parse --short HEAD)
+$buildDir = Join-Path $rootDir 'build'
+$scriptsDir = Join-Path $buildDir 'scripts'
+$toolsDir = Join-Path $buildDir 'tools'
+$artifactsDir = Join-Path $buildDir 'artifacts'
 
-dotnet "$cakeAssemblyPath" "$cakeScript" "--gitRemote=$gitRemote" "--srcDir=$srcDir" "--artifactsDir=$artifactsDir" "--commitHash=$commitHash" $args
+$cakeAssemblyPath = Get-ToolAssemblyPath $args $toolsDir 'cake.tool' '--cakePackageVersion=' 'dotnet-cake' 'cake.dll'
+$cakeScript = Join-Path $scriptsDir 'cake.cs'
+$gitRemote = $(git remote get-url origin) -replace '.git$', ''
+$commitHash = $(git rev-parse --short HEAD)
+
+$cakeArgs = @("$cakeScript", "--gitRemote=$gitRemote", "--srcDir=$srcDir", "--artifactsDir=$artifactsDir", "--commitHash=$commitHash") + $args
+& $cakeAssemblyPath $cakeArgs
--- build/scripts/bootstrap.sh
+++ build/scripts/bootstrap.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-function findPackageAssembly {
+function findToolAssembly {
     local resultvar=$1
     local dir=$2
     local packageName=$3
     local packageVersion=$4
-    local assemblyName=$5
-    local packageAssembly=$(find "$dir/$packageName/$packageVersion" -name "$assemblyName" 2>/dev/null)
-    eval $resultvar="$packageAssembly"
+    local toolName=$5
+    local assemblyName=$6
+    local packageAssembly=$(find "$dir/$packageName/$packageVersion" -iname "$assemblyName" 2>/dev/null)
+    if [ ! -f "$packageAssembly" ]; then
+        eval $resultvar="$packageAssembly"
+    else
+        local toolAssembly=$(find "$dir/$packageName/$packageVersion" -iname "$toolName" 2>/dev/null)
+        eval $resultvar="$toolAssembly"
+    fi
 }
 
-function installPackage {
+function installTool {
     local dir=$1
     local packageName=$2
     local packageVersion=$3
     echo "Installing '$packageName' version '$packageVersion' in '$dir'..."
-    mkdir -p $dir
-    local contents='<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><TargetFramework>netstandard1.5</TargetFramework></PropertyGroup></Project>'
-    local proj="$dir/prj.csproj"
-    echo $contents > $proj
-    dotnet add "$proj" package $packageName --version $packageVersion --package-directory $dir
+    local toolDir="$dir/$packageName/$packageVersion"
+    if [ -d "$toolDir" ]; then
+        rm -rf $toolDir
+    fi
+    mkdir -p $toolDir
+    dotnet tool install $packageName --tool-path $toolDir --version $packageVersion &>/dev/null
 }
 
-function getPackageAssemblyPath {
+function getToolAssemblyPath {
     local resultvar=$1
     local scriptArgsName=$2[@]
-    local packagesDir=$3
+    local toolsDir=$3
     local packageName=$4
     local packageVersionArgSwitch=$5
-    local assemblyName=$6
+    local toolName=$6
+    local assemblyName=$7
     local scriptArgs=("${!scriptArgsName}")
     for item in ${scriptArgs[@]}; do
         if [[ $item == $packageVersionArgSwitch* ]]; then
@@ -39,27 +47,31 @@ function getPackageAssemblyPath {
         local packageVersionSearchUrl="https://api-v2v3search-0.nuget.org/autocomplete?id=$packageName"
         local packageVersion=$(curl --silent $packageVersionSearchUrl | jq .data[-1] | sed -e 's/^"//' -e 's/"$//')
     fi
-    findPackageAssembly assemblyPath $packagesDir $packageName $packageVersion $assemblyName
+    findToolAssembly assemblyPath $toolsDir $packageName $packageVersion $toolName $assemblyName
     if [ ! -f "$assemblyPath" ]; then
-        installPackage $packagesDir $packageName $packageVersion
-        findPackageAssembly assemblyPath $packagesDir $packageName $packageVersion $assemblyName
+        installTool $toolsDir $packageName $packageVersion
+        findToolAssembly assemblyPath $toolsDir $packageName $packageVersion $toolName $assemblyName
         if [ ! -f "$assemblyPath" ]; then
-            echo "Cannot find '$assemblyName' in directory '$packagesDir' for package '$packageName' of version '$packageVersion'"
+            echo "Cannot find '$toolName' and/or '$assemblyName' in directory '$toolsDir' for package '$packageName' of version '$packageVersion'"
             exit 1
         fi
     fi
     eval $resultvar="$assemblyPath"
 }
 
-scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-rootDir=$scriptDir; while [ $(ls -Ald "$rootDir/.git" 2>/dev/null | wc -l) -eq 0 ]; do rootDir="$rootDir/.."; done
-buildScriptPackagesDir="$rootDir/build/tools"
-allArgs=$@
-getPackageAssemblyPath cakeAssemblyPath allArgs $buildScriptPackagesDir 'cake.coreclr' '--cakePackageVersion=' 'Cake.dll'
-cakeScript="$scriptDir/cake.cs"
-gitRemote=$(git remote get-url origin | sed -e 's/.git$//')
+externalArgs="$@"
+
+rootDir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd); while [ $(ls -Ald "$rootDir/.git" 2>/dev/null | wc -l) -eq 0 ]; do rootDir="$rootDir/.."; done; rootDir=$(cd "$rootDir" && pwd)
 srcDir="$rootDir/src"
-artifactsDir="$rootDir/build/artifacts"
+buildDir="$rootDir/build"
+scriptsDir="$buildDir/scripts"
+toolsDir="$buildDir/tools"
+artifactsDir="$buildDir/artifacts"
+
+getToolAssemblyPath cakeAssemblyPath externalArgs $toolsDir 'cake.tool' '--cakePackageVersion=' 'dotnet-cake' 'cake.dll'
+cakeScript="$scriptsDir/cake.cs"
+gitRemote=$(git remote get-url origin | sed -e 's/.git$//')
 commitHash=$(git rev-parse --short HEAD)
 
-dotnet "$cakeAssemblyPath" "$cakeScript" "--gitRemote=$gitRemote" "--srcDir=$srcDir" "--artifactsDir=$artifactsDir" "--commitHash=$commitHash" "$@"
+internalArgs=("$cakeAssemblyPath" "$cakeScript" "--gitRemote=$gitRemote" "--srcDir=$srcDir" "--artifactsDir=$artifactsDir" "--commitHash=$commitHash")
+"${internalArgs[@]}" $externalArgs
