b306e7e96f022348dccbb7bc6685de3e2011c553
Luigi Berrettini <luigi.berrettini@gmail.com>
2020-06-14 20:34:18 +0200

    Allow not to output space before RFC 3164 MSG part

--- src/NLog.Targets.Syslog/Policies/ContentPolicySet.cs
+++ src/NLog.Targets.Syslog/Policies/ContentPolicySet.cs
@@ -6,20 +6,20 @@ using NLog.Targets.Syslog.Settings;
 
 namespace NLog.Targets.Syslog.Policies
 {
-    internal class PlainContentPolicySet : PolicySet
+    internal class ContentPolicySet : PolicySet
     {
         private const string NonSpaceOrPrintUsAscii = @"[^\u0020-\u007E]";
         private const string QuestionMark = "?";
-        private const string NonAlphaNumericFirstChar = @"^([a-zA-Z0-9])(.*)$";
-        private const string PrefixWithSpaceReplacement = " $1$2";
+        private const string AlphaNumericFirstChar = @"^([a-zA-Z0-9])(.*)$";
+        private const string PrefixWithSpaceReplacement = " $1$2"; // content first char must be non alphanumeric
 
-        public PlainContentPolicySet(EnforcementConfig enforcementConfig)
+        public ContentPolicySet(EnforcementConfig enforcementConfig)
         {
             AddPolicies(new List<IBasicPolicy<string, string>>
             {
                 new TransliteratePolicy(enforcementConfig),
                 new ReplaceKnownValuePolicy(enforcementConfig, NonSpaceOrPrintUsAscii, QuestionMark),
-                new ReplaceKnownValuePolicy(enforcementConfig, NonAlphaNumericFirstChar, PrefixWithSpaceReplacement)
+                new ReplaceKnownValuePolicy(enforcementConfig, AlphaNumericFirstChar, PrefixWithSpaceReplacement)
             });
         }
     }
